751322e36673806a60a504dea8cd56f6fc7a5c4c
OA Wu <comdan66@gmail.com>
2018-07-19 15:42:06 +0800

    fix code.....w2

--- sys/cmd/deploy.php
+++ sys/cmd/deploy.php
@@ -3,5 +3,5 @@ $connection = ssh2_connect('kerker.tw', 22);
 
 $stream = ssh2_exec($connection, 'ls');
 echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
-var_dump ($stream);
+var_dump ($strea);
 exit ();
